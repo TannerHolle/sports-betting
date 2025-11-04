b7e851b9089f9b2cd9e2d5ff7baa7f78cbed5d46
Tanner Holle <tannerholle96@gmail.com>
2025-11-03 18:25:04 -0700

    rows for bet history

--- src/components/BetHistory.vue
+++ src/components/BetHistory.vue
@@ -556,9 +556,15 @@ export default {
 }
 
 .bets-list {
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr;
   gap: 1rem;
+}
+
+@media (min-width: 1084px) {
+  .bets-list {
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 
 .bet-card {
